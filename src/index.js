39032cba8b542627f16ec941180f0eed643a7ea2
melyaua <igor.melnychenko@tmx-learning.com>
2020-10-09 14:25:01 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if ( str.length % 2 !== 0) return false;
+    let arr = str.split('');
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let count = 0; count < bracketsConfig.length; count++) {
+            if(arr[i] === bracketsConfig[count][0] && arr[i + 1] === bracketsConfig[count][1]) {
+                arr.splice(i, 2);
+                i = -1;
+            }
+        }
+    }
+    return !arr.length;
 }
